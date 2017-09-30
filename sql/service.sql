7fa5b28f0e66f4e13909c05e07c78ef3745e926a
fitnr <fitnr@fakeisthenewreal>
2017-09-30 09:25:18 -0400

    convert Speed and Service functions to pgsql

--- sql/service.sql
+++ sql/service.sql
@@ -1,26 +1,54 @@
-SELECT
-  start_date, end_date
-FROM start_date INTO @start_date, @end_date;
+-- Counts of scheduled buses, observed buses, per route stop and direction
 
--- 2.6 million rows in 12m on xl
-INSERT perf_service
+CREATE OR REPLACE FUNCTION get_service (start_date DATE, term INTERVAL)
+    RETURNS TABLE(
+        start date,
+        term interval,
+        route_id text,
+        direction_id int,
+        stop_id text,
+        weekend int,
+        period int,
+        hours int,
+        scheduled int,
+        observed int
+    ) AS $$
     SELECT
-        @start_date AS month,
+        start_date AS start,
+        term,
         route_id,
         direction_id,
         stop_id,
-        WEEKDAY(sh.`date`) >= 5 OR h.`holiday` IS NOT NULL AS weekend,
+        EXTRACT(isodow FROM "datetime") > 5 OR h.holiday IS NOT NULL AS weekend,
         day_period_hour(hour) AS period,
         SUM(1),
         SUM(sh.pickups),
         COALESCE(SUM(a.observed), 0)
     FROM schedule_hours AS sh
-        LEFT JOIN adherence AS a USING (date, rds_index, hour)
+        LEFT JOIN adherence AS a USING (date, route_id, direction_id, stop_id, hour)
         LEFT JOIN ref_holidays h USING (date)
         JOIN ref_rds USING (rds_index)
-    WHERE sh.date BETWEEN @start_date AND @end_date
+    WHERE sh.date BETWEEN start_date AND start_date + term
         AND sh.pickups > 0
         AND NOT exception
     GROUP BY rds_index,
         weekend,
-        period;
+        period
+    $$
+LANGUAGE SQL STABLE;
+
+CREATE OR REPLACE FUNCTION get_service (start_date DATE)
+    RETURNS TABLE(
+        month date,
+        route_id text,
+        direction_id int,
+        stop_id text,
+        weekend int,
+        period int,
+        hours int,
+        scheduled int,
+        observed int
+    ) AS $$
+    SELECT * FROM get_service(start_date, INTERVAL '1 MONTH' - INTERVAL '1 DAY')
+    $$
+LANGUAGE SQL STABLE;
--- sql/speed.sql
+++ sql/speed.sql
@@ -71,3 +71,56 @@ CREATE OR REPLACE FUNCTION get_speed (start_date date)
     SELECT * FROM get_speed(start_date, INTERVAL '1 MONTH' - INTERVAL '1 DAY')
     $$
 LANGUAGE SQL STABLE;
+
+CREATE OR REPLACE FUNCTION get_speed (start_date date, term interval, route text)
+    RETURNS TABLE(
+        "month" date,
+        route_id text,
+        direction_id int,
+        stop_id text,
+        weekend int,
+        period int,
+        distance numeric,
+        travel_time numeric,
+        count int
+    )
+    AS $$
+    SELECT
+        start_date,
+        route_id,
+        direction_id,
+        stop_id,
+        weekend::int,
+        period,
+        ROUND(SUM(dist)::numeric, 3) distance,
+        ROUND(EXTRACT(epoch from SUM(elapsed))::numeric, 1) travel_time,
+        COUNT(*)::integer count
+    FROM (
+        SELECT
+            day_period(call_time::time) AS period,
+            EXTRACT(DOW FROM call_time) >= 5 OR h.holiday IS NOT NULL weekend,
+            route_id,
+            direction_id,
+            stop_id,
+            call_time - LAG(call_time) OVER (run) AS elapsed,
+            shape_dist_traveled - LAG(shape_dist_traveled) OVER (run) AS dist
+        FROM calls
+            LEFT JOIN gtfs_trips USING (trip_id, direction_id, route_id)
+            LEFT JOIN gtfs_stop_times USING (trip_id, stop_id)
+            LEFT JOIN stat_holidays h ON (h.date = (call_time AT TIME ZONE 'US/Eastern')::DATE)
+        WHERE source = 'I'
+            AND (call_time AT TIME ZONE 'US/Eastern')::DATE BETWEEN
+                start_date AND start_date + term
+            AND route_id = route
+        WINDOW run AS (PARTITION BY vehicle_id, trip_id ORDER BY call_time ASC)
+    ) raw
+    WHERE elapsed > '00:00'::INTERVAL
+        AND dist > 0
+    GROUP BY
+        route_id,
+        direction_id,
+        stop_id,
+        weekend,
+        period
+    $$
+LANGUAGE SQL STABLE;
