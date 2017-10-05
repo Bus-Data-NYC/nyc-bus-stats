1b1aedf0d720698907ad38b9e706d13a7e8343ef
fitnr <fitnr@fakeisthenewreal>
2017-10-05 16:57:27 -0400

    draft cewt

--- sql/cewt.sql
+++ sql/cewt.sql
@@ -1,21 +1,38 @@
 -- cewt
-SELECT
-    route_id
-    direction_id,
-    stop_id,
-    day_period(obs.datetime::time) AS period,
-    (EXTRACT(isodow FROM obs.datetime) > 5 OR holiday IS NOT NULL) AS weekend,
-    obs.headway AS headway_obs,
-    sched.headway AS headway_sched,
-    COUNT(*) AS count,
-    COUNT(NULLIF(false, a.headway_obs > a.headway_sched)) AS count_cewt,
-    ROUND(AVG(EXTRACT(EPOCH FROM a.headway_obs - a.headway_sched)::NUMERIC / 60.), 2) AS cewt_avg
-FROM
-    stat_headway_observed obs
-    INNER JOIN stat_headway_scheduled sched USING (trip_id, route_id, direction_id, stop_id)
-    LEFT JOIN stat_holidays AS h ON (h.date = obs.datetime::date)
-WHERE 
-    obs.datetime::date = sched.datetime::date
-    AND sched.trip_id IS NOT NULL
-    AND obs.datetime BETWEEN $1 and $2
-GROUP BY route_id, 4, 5
+CREATE OR REPLACE FUNCTION get_cewt (start date, term interval)
+    RETURNS TABLE(
+        start date,
+        term interval,
+        route_id text,
+        direction_id int,
+        stop_id text,
+        weekend int,
+        period int,
+        count int,
+        count_cewt int,
+        cewt_avg numeric(10, 2)
+    )
+    AS $$
+    SELECT
+        start,
+        term,
+        route_id
+        direction_id,
+        stop_id,
+        (EXTRACT(isodow FROM obs.datetime) > 5 OR holiday IS NOT NULL)::int AS weekend,
+        obs.period,
+        obs.headway AS headway_obs,
+        sched.headway AS headway_sched,
+        COUNT(*) AS count,
+        COUNT(NULLIF(false, a.headway_obs > a.headway_sched)) AS count_cewt,
+        ROUND(AVG(EXTRACT(EPOCH FROM a.headway_obs - a.headway_sched)::NUMERIC / 60.), 2) AS cewt_avg
+    FROM
+        stat_headway_observed obs
+        INNER JOIN stat_headway_scheduled sched USING (trip_id, stop_id, date)
+        LEFT JOIN stat_holidays AS h USING ("date")
+    WHERE 
+        sched.trip_id IS NOT NULL
+        AND obs.datetime BETWEEN $1 and $2
+    GROUP BY route_id, 4, 5
+    $$
+LANGUAGE SQL STABLE;
