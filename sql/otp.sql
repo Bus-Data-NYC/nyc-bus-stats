5d79aef4f7cbb5b222ec95ce572806beb154ad3c
fitnr <fitnr@fakeisthenewreal>
2017-10-06 09:06:32 -0400

    draft otp function

--- sql/otp.sql
+++ sql/otp.sql
@@ -1,25 +1,56 @@
-INSERT perf_otp SELECT
-    date_trunc('month', service_date)::date AS month,
-    route_id,
-    direction_id,
-    stop_id,
-    (EXTRACT(isodow FROM ****) >= 6 OR h.holiday IS NOT NULL) AS weekend,
-    day_period(***:time) AS period,
-    COALESCE(SUM(early), 0) early,
-    COALESCE(SUM(on_time), 0) on_time,
-    COALESCE(SUM(late), 0) late
+CREATE OR REPLACE FUNCTION get_otp (start date, term interval)
+    RETURNS TABLE(
+        start date,
+        term interval,
+        route_id text,
+        direction_id int,
+        stop_id text,
+        weekend int,
+        period int,
+        early int,
+        on_time int,
+        late int
+    )
+    AS $$
+    SELECT
+        "start",
+        "term",
+        route_id,
+        direction_id,
+        stop_id,
+        (EXTRACT(isodow FROM call_time at time zone 'US/Eastern') > 5 OR h.holiday IS NOT NULL)::int AS weekend,
+        day_period(call_time at time zone 'US/Eastern') AS period,
+        COUNT(NULLIF(false, deviation < interval '-60'))::int AS early,
+        COUNT(NULLIF(false, deviation >= interval '-60' AND deviation <= interval '300'))::int AS on_time,
+        COUNT(NULLIF(false, deviation > interval '300'))::int AS late
+    FROM calls
+        LEFT JOIN stat_holidays h ON (trip_start_date = h.date)
+    WHERE trip_start_date >= "start"
+        AND trip_start_date < "start" + "term"
+    GROUP BY
+        route_id,
+        direction_id,
+        stop_id,
+        EXTRACT(isodow FROM call_time at time zone 'US/Eastern') > 5 OR h.holiday IS NOT NULL,
+        day_period(call_time at time zone 'US/Eastern')
+    $$
+LANGUAGE SQL STABLE;
 
-    SUM(IF(deviation < -60, 1, 0)) AS early,
-    SUM(IF(deviation >= -60 AND deviation <= 300, 1, 0)) AS on_time,
-    SUM(IF(deviation > 300, 1, 0)) AS late,
-FROM calls
-    LEFT JOIN ref_holidays h ON (service_date = date)
-WHERE
-    (call_time + deviation)::date BETWEEN $1 AND $2
-GROUP BY
-    date_trunc('month', service_date)::date,
-    route_id,
-    direction_id,
-    stop_id,
-    weekend,
-    period;
+CREATE OR REPLACE FUNCTION get_otp (start date)
+    RETURNS TABLE(
+        month date,
+        route_id text,
+        direction_id int,
+        stop_id text,
+        weekend int,
+        period int,
+        early int,
+        on_time int,
+        late int
+    )
+    AS $$
+    SELECT start AS month, route_id, direction_id, stop_id,
+        weekend, period, early, on_time, late
+    FROM get_otp(start, '1 MONTH'::INTERVAL)
+    $$
+LANGUAGE SQL STABLE;
